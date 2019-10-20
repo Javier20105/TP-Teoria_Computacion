ecaf48acafd2a64553cdbeaea515f95c5600a65f
Javier <xavierescobar3335@gmail.com>
2019-10-20 13:05:18 -0300

    Completado quitar producciones nulleables

--- ejercicio 2/src/App.scala
+++ ejercicio 2/src/App.scala
@@ -5,16 +5,16 @@ import java.io._
 
 object App {
 
-  def crearProducciones(s: Array[String]): Set[Produccion] = {
-    if (s.size == 0) {
-      Set()
-    } else {
-      val partesProd = s(s.size - 1).split("->")
-      crearProducciones(s.take(s.size - 1)) + new Produccion(partesProd(0).charAt(0), partesProd(1))
-    }
-  }
-
   def importarGramatica(ruta: String): Gramatica = {
+
+    def crearProducciones(s: Array[String]): Set[Produccion] = {
+      if (s.size == 0) {
+        Set()
+      } else {
+        val partesProd = s(s.size - 1).split("->")
+        crearProducciones(s.take(s.size - 1)) + new Produccion(partesProd(0).charAt(0), partesProd(1))
+      }
+    }
     val lineas = Source.fromFile(ruta).getLines()
     val terminales = Set() ++ lineas.next.toList
     val variables = Set() ++ lineas.next.toList
@@ -35,28 +35,68 @@ object App {
       producciones.filter(_.esEpsilon).map(_.variable)
     }
 
-    def casoInductivo(anterior:Set[Char], producciones:Set[Produccion]):Set[Char] = {
-      anterior ++ producciones.filter(esNulleable(_,anterior)).map(_.variable)
+    def casoInductivo(anterior: Set[Char], producciones: Set[Produccion]): Set[Char] = {
+      anterior ++ producciones.filter(esNulleable(_, anterior)).map(_.variable)
     }
 
     def recursiva(anterior: Set[Char], actual: Set[Char], producciones: Set[Produccion]): Set[Char] = {
       if (anterior == actual) {
         return actual
       } else {
-        val siguiente = casoInductivo(actual,producciones)
+        val siguiente = casoInductivo(actual, producciones)
         recursiva(actual, siguiente, producciones)
       }
     }
-    
+
     val paso0 = casoBase(producciones)
-    val paso1 =casoInductivo(paso0,producciones)    
+    val paso1 = casoInductivo(paso0, producciones)
     recursiva(paso0, paso1, producciones)
+  }
+
+  def eliminarProduccionesEpsilon(g: Gramatica, nulleables: Set[Char]): Gramatica = {
+
+    //"-------------------------------------------------------------------"
+
+    def encontrarPrimero(s: String, c: Char): Int = {
+      s.indexOf(c)
+    }
+    def quitar(s: String, index:Int): String = {
+      s.slice(0, index) + s.slice(index + 1, s.size)
+    }
+
+    def nulleablesEnProduccion(p: Produccion, c: List[Char]): List[Char] = {
+      p.cadena.toList.filter(c.contains(_))
+    }
+
+    def recursiva(p: Produccion, nulleables: List[Char],index:Int): Set[Produccion] = {
+      if (nulleables.size == 0) {
+        Set(p)
+      } else {
+        val n = nulleables.tail
+        val i = index + encontrarPrimero(p.cadena.slice(index,p.cadena.size), nulleables.head)
+        recursiva(p, n,i) ++ recursiva(new Produccion(p.variable, quitar(p.cadena, i)), n,i)
+
+      }
+
+    }
+
+    def produccionSinEpsilon(p: Produccion, nulleables: List[Char]): Set[Produccion] = {
+      recursiva(p, nulleablesEnProduccion(p, nulleables),0)
+    }
+
+    val resul = g.producciones.toList.map(produccionSinEpsilon(_, nulleables.toList))
+    print("Resul: " + resul)
+
+    null
   }
 
   def main(args: Array[String]): Unit = {
 
     val g = importarGramatica("Input/input")
+    val nulleables = descubrirNulleables(g.producciones)
     println(g)
-    println("Nulleables: " + descubrirNulleables(g.producciones))
+    println("Nulleables: " + nulleables)
+    val sinEpsilon = eliminarProduccionesEpsilon(g, nulleables)
+
   }
 }
--- ejercicio 2/src/Modelo/Produccion.scala
+++ ejercicio 2/src/Modelo/Produccion.scala
@@ -1,6 +1,6 @@
 package Modelo
 
-class Produccion(v: Char, c: String) {
+case class Produccion(v: Char, c: String) {
   val variable = v
   val cadena = c
   override def toString = { s"$variable -> $cadena" }
