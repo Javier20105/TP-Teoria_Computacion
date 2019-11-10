b2c5a22fac537b60725da90b90a9c5828df7b03f
Javier20105 <xavierescobar3335@gmail.com>
2019-11-10 15:21:14 -0300

    creados casos de test para el metodo perteneceA

--- Ejercicio2/src/test/scala/CYKTest.scala
+++ Ejercicio2/src/test/scala/CYKTest.scala
@@ -15,19 +15,24 @@ class CYKTest extends UnitSpec {
     val cadena = "abab"
     val matriz = List(List(Set('A'), Set('B'), Set('A'), Set('B')),   List(Set('D'), Set(), Set('D')),     List(Set(), Set('C')),    List(Set('S')))
     assert(matriz == CYK.generarMatriz(cadena,g_importada))
+    assert(CYK.perteneceA(cadena,g_importada))
 
 
     //para cualquier otra parlabras no debe estar la S
     val cadena1 = "bbaaba"
     val matriz1 = List(List(Set('B'), Set('B'), Set('A'), Set('A'), Set('B'), Set('A')),     List(Set(), Set(), Set(), Set('D'), Set()),     List(Set(), Set(), Set(), Set()),      List(Set(), Set(), Set()),     List(Set(), Set()),     List(Set()))
     assert(matriz1 == CYK.generarMatriz(cadena1,g_importada))
+    assert(!CYK.perteneceA(cadena1,g_importada))
+
     val cadena2 = "aab"
     val matriz2 = List(List(Set('A'), Set('A'), Set('B')),      List(Set(), Set('D')),      List(Set()))
     assert(matriz2 == CYK.generarMatriz(cadena2,g_importada))
-   /* val matriz2 = CYK.generarMatriz(cadena2, g_importada).reverse
-    assert(!matriz2.head.head.contains('S'))
-    assert(matriz2.length == cadena2.length)
-    for (n <- 0 to (cadena2.length-1)) assert(matriz2(n).length == n+1)*/
+    assert(!CYK.perteneceA(cadena2,g_importada))
+
+    /* val matriz2 = CYK.generarMatriz(cadena2, g_importada).reverse
+     assert(!matriz2.head.head.contains('S'))
+     assert(matriz2.length == cadena2.length)
+     for (n <- 0 to (cadena2.length-1)) assert(matriz2(n).length == n+1)*/
 
 
   }
@@ -38,25 +43,36 @@ class CYKTest extends UnitSpec {
     val cadena = "c"
     val matriz = List(List(Set('S')))
     assert(matriz == CYK.generarMatriz(cadena,g_importada))
+    assert(CYK.perteneceA(cadena,g_importada))
+
 
     val cadena1 = "acb"
     val matriz1 = List(List(Set('A'), Set('S'), Set('B')),      List(Set(), Set('C')),      List(Set('S')))
     assert(matriz1 == CYK.generarMatriz(cadena1,g_importada))
+    assert(CYK.perteneceA(cadena1,g_importada))
+
     val cadena2 = "aacbb"
     val matriz2 = List(List(Set('A'), Set('A'), Set('S'), Set('B'), Set('B')),      List(Set(), Set(), Set('C'), Set()),      List(Set(), Set('S'), Set()),      List(Set(), Set('C')),      List(Set('S')))
     assert(matriz2 == CYK.generarMatriz(cadena2,g_importada))
+    assert(CYK.perteneceA(cadena2,g_importada))
+
 
     val cadena3 = "aaacbbb"
     val matriz3 = List(List(Set('A'), Set('A'), Set('A'), Set('S'), Set('B'), Set('B'), Set('B')),      List(Set(), Set(), Set(), Set('C'), Set(), Set()),      List(Set(), Set(), Set('S'), Set(), Set()),      List(Set(), Set(), Set('C'), Set()),      List(Set(), Set('S'), Set()),      List(Set(), Set('C')),      List(Set('S')))
     assert(matriz3 == CYK.generarMatriz(cadena3,g_importada))
+    assert(CYK.perteneceA(cadena3,g_importada))
+
     //para una palabra que no pertenesca al lenguaje no debe estar la S en el ultimo nivel
     val cadena4 = "aaabbb"
     val matriz4 = List(List(Set('A'),Set('A'),Set('A'),Set('B'),Set('B'),Set('B')),     List(Set(),Set(),Set(),Set(),Set()),    List(Set(),Set(),Set(),Set()),     List(Set(),Set(),Set()),      List(Set(),Set()),     List(Set())      )
     assert(matriz4 == CYK.generarMatriz(cadena4,g_importada))
+    assert(!CYK.perteneceA(cadena4,g_importada))
 
     val cadena5 = "bacba"
     val matriz5 = List(List(Set('B'),Set('A'),Set('S'),Set('B'),Set('A')), List(Set(),Set(),Set('C'),Set()), List(Set(),Set('S'),Set()),  List(Set(),Set()),List(Set())          )
     assert(matriz5 == CYK.generarMatriz(cadena5,g_importada))
+    assert(!CYK.perteneceA(cadena5,g_importada))
+
 
   }
 
@@ -65,11 +81,39 @@ class CYKTest extends UnitSpec {
     val cadena = "aaab"
     val matriz = List(List(Set('A'), Set('A'), Set('A'), Set('B')),      List(Set('A', 'D'), Set('A', 'D'), Set()),      List(Set('E', 'A', 'D'), Set('S')),      List(Set('S')))
     assert(matriz == CYK.generarMatriz(cadena,g_importada))
+    assert(CYK.perteneceA(cadena,g_importada))
+
 
     //si la cadena empiesa con b no pertence al lenguaje luego la Sno esta en el ultimo nivel
     val cadena1 = "baaa"
     val matriz1 = List(List(Set('B'), Set('A'), Set('A'), Set('A')),      List(Set(), Set('A', 'D'), Set('A', 'D')),      List(Set(), Set('E', 'A', 'D')),      List(Set()))
     assert(matriz1== CYK.generarMatriz(cadena1,g_importada))
+    assert(!CYK.perteneceA(cadena1,g_importada))
+
+  }
+
+  "CYK" should "no aceptar una cadena si contiene un caracter que no es terminal" in {
+    val g_importada1 = Importador.importarGramatica(rutaGenerarMatriz + "caso1_palabraenLenguaje")
+    val g_importada2 = Importador.importarGramatica(rutaGenerarMatriz + "caso2_masDeUnaPalabraEnLenguaje")
+    val g_importada3 = Importador.importarGramatica(rutaGenerarMatriz + "caso3_redundanciaDeCadena")
+
+    //ninguna es terminal
+    val cadena = "defghi"
+    assert(!CYK.perteneceA(cadena,g_importada1))
+    assert(!CYK.perteneceA(cadena,g_importada2))
+    assert(!CYK.perteneceA(cadena,g_importada3))
+
+    //alguna no es terminal
+    val cadena1 = "aaadbbb"
+    assert(!CYK.perteneceA(cadena1,g_importada1))
+    assert(!CYK.perteneceA(cadena1,g_importada2))
+    assert(!CYK.perteneceA(cadena1,g_importada3))
+
+    
+
+
+
+
   }
 
 
